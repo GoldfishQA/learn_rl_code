a3681af12b049593fca3e019c19dd0498edc203c
GoldfishQA <qiurui961016@yahoo.co.jp>
2018-05-30 16:00:45 +0900

    finish 1

--- rl_test.py
+++ rl_test.py
@@ -33,10 +33,10 @@ def choose_action(state, q_table):
 def get_env_feedback(S, A):
 	if A == 'right':
 		if S ==N_STATES - 2:
-			S = 'terminal'
+			S_ = 'terminal'
 			R = 1
 		else:
-			S = S + 1
+			S_ = S + 1
 			R = 0
 	else:
 		R = 0
@@ -45,3 +45,46 @@ def get_env_feedback(S, A):
 		else:
 			S_ = S -1
 	return S_, R
+
+def update_env(S, episode, step_counter):
+	env_list = ['-']*(N_STATES-1) + ['T']
+	if S == 'terminal':
+		interaction = 'Episode %s: total_steps = %s' % (episode+1, step_counter)
+		print('\r{}'.format(interaction), end = '')
+		time.sleep(2)
+		print('\r                   ', end = '')
+	else:
+		env_list[S] = 'o'
+		interaction = ''.join(env_list)
+		print('\r{}'.format(interaction), end = '')
+		time.sleep(FRESH_TIME)
+
+def rl():
+	q_table = build_q_table(N_STATES, ACTIONS)
+	for episode in range(MAX_EPISODES):
+		step_counter = 0
+		S = 0
+		is_terminated = False
+		update_env(S, episode, step_counter)
+		while not is_terminated:
+
+			A = choose_action(S, q_table)
+			S_, R = get_env_feedback(S, A)
+			q_predict = q_table.ix[S, A]
+			if S_ != 'terminal':
+				q_target = R + LAMBDA * q_table.iloc[S_, :].max()
+			else:
+				q_target = R
+				is_terminated = True
+
+			q_table.ix[S, A] += ALPHA * (q_target - q_predict)
+			S = S_
+
+			update_env(S, episode, step_counter + 1)
+			step_counter += 1
+	return q_table
+
+if __name__ == "__main__":
+	q_table = rl()
+	print('\r\nQ-table:\n')
+	print(q_table)
